03e034b7b1582b8470e7f29038d6749b9e53acbe
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2017-04-20 23:25:21 +0300

    Add WowRealm pojo

---
+++ app/src/main/java/com/mhmt/wowrealmstatus/WowRealm.java
@@ -0,0 +1,17 @@
+package com.mhmt.wowrealmstatus;
+
+
+public class WowRealm {
+
+  private String type;
+  private String population;
+  private boolean queue;
+  private boolean status;
+  private String name;
+  private String slug;
+  private String battlegroup;
+  private String locale;
+  private String timezone;
+//  private RealmList<String> connectedRealms;
+
+}
